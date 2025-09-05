3b691e3f961a428b0fdc73fe5972d00127acb294
faisal <faisalfaroqq1873@gmail.com>
2025-09-05 04:19:12 -0700

    create a file

---
+++ app.js
@@ -0,0 +1,261 @@
+// chap alert start.
+//Q 1.
+// alert()
+//Q2.
+// alert("please enter a valid password");
+//Q3.
+// alert("welcometo js Land \n Happy Coding");
+// Q4.
+// alert("Welcome to js Land");
+// alert("Happy Coding");
+// Q5.
+// alert("I can run js through my web browser console");
+// chap 2 start.
+// Q1.
+// var firstname = "muhammad";
+// console.log(firstname);
+// Q2.
+// var fullname = "muhammad faisal";
+// console.log(fullname);
+// Q3.a
+// var message = "hello world";
+// console.log(message);
+// Q3.b
+// var message = "faisal";
+// var message = "hello world";
+// alert(message);
+// Q5.
+// var  a = "PIZZA \n PIZ \n PIZ \n PI \n P"
+// alert(a)
+// Q6.
+// var email  = "My email address is example @ exampel.com";
+// alert(email);
+// Q7.
+// var book = "I am tring to learn from the book a smater way to learn javascript";
+// alert(book);
+// Q8.
+// var a = "Yah! I can Write HTML content through javascript";
+// document.write(a);
+// chap 3 start.
+// Q1.
+//  var num = "I am 15 Years old";
+//  alert(num);
+// Q2.
+// var num = "you have visited this sites 14 times";
+// alert(num);
+// Q3.
+//  var birthyear = "my birth year is 1990 \n Data type of my decalerd variable is number";
+//  alert(birthyear);
+// Q4.
+//  var a = " John Doe";
+//  var b = "on XYZ clothing store";
+//  var c = " 5 T-shirt(s)";
+//  document.write(a + c + b);
+// Q4.
+// var name = "Ali", age = 20, city = "Karachi";
+// document.write(name + age + city);
+// Q5.
+// legal tariqa.
+// var myname;
+// var $price;
+// var _value;
+// var user123;
+// var my_variables;
+// ILLegal Tariqa.
+// var 123name;
+// var my name;
+// var my-name;
+// var var;
+// var alert;
+// Q6.
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variables names can only contain letter, $ and _ For example $my_1stVariable <br>");
+// document.write("Variables must begin with a letter, $ or _ . For example $name, _name or n <br>");
+// document.write(" Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS keywords");
+// chap 5 start.
+//Q1.
+// var num1 = "3" , num2 = "5" , num3 = "8";
+// document.write("Sum of "+ num1 +"  and "+ num2 +" is "+ num3 +"");
+// Q2.
+// var num1 = 1 , num2 = 2;
+// console.log(num1+num2);
+// var num1 = 20 , num2 = 10;
+// console.log(num1-num2);
+// var num1 = 5, num2 = 3;
+// console.log(num1*num2);
+// var num1 = 5, num2 = 5;
+// console.log(num1/num2);
+// Q3.
+// a)
+// var  myvar;
+// // b)
+// document.write("value after variable declaration is "+ myvar +" <br>");
+// // c)
+// myvar = 5;
+// // d)
+// document.write("Initial value: "+ myvar +" <br>");
+// // e)
+// myvar++;
+// // f)
+// document.write("value after imcrement is: "+ myvar +"<br>");
+// // g)
+// myvar = myvar + 7;
+// // h)
+// document.write("value after addition is "+ myvar + "<br>");
+// // I)
+// myvar--;
+// // j)
+// document.write("value after decrement is: "+myvar+"<br>");
+// // k)
+// var remainder = myvar % 3;
+// // L)
+// document.write("The remainder is: "+ remainder +"");
+// Q4)
+// var ticketprice = 600;
+// var tickets = 5;
+// var totalcost = ticketprice * 5;
+// document.write("Total cost to buy "+tickets+" ticket ta a movie is "+totalcost+"PKR");
+// Q5)
+// var number = 4;
+// document.write("Table of "+ number +"<br>");
+// document.write(number + " x 1 = " + (number * 1) + "<br>");
+// document.write(number + " x 2 = " + (number * 2) + "<br>");
+// document.write(number + " x 3 = " + (number * 3) + "<br>");
+// document.write(number + " x 4 = " + (number * 4) + "<br>");
+// document.write(number + " x 5 = " + (number * 5) + "<br>");
+// document.write(number + " x 6 = " + (number * 6) + "<br>");
+// document.write(number + " x 7 = " + (number * 7) + "<br>");
+// document.write(number + " x 8 = " + (number * 8) + "<br>");
+// document.write(number + " x 9 = " + (number * 9) + "<br>");
+// document.write(number + " x 10 = " + (number * 10) + "<br>");
+// Q6)
+// a)
+// var celsius = 25;
+// // b)
+// var fahrenheitResult = (celsius * 9/5) + 32;
+// document.write(+ celsius + "°C is " + fahrenheitResult + "°F<br>");
+// // c)
+// var fahrenheit = 70;
+// // d)
+// var celsiusResult = (fahrenheit - 32) * 5/9;
+// document.write( + fahrenheit + "°F is " + celsiusResult + "°C<br>");
+// Q7) 
+// Step 1: Store values in variables
+// a)
+//  var priceitem1 = 650;
+// //b)
+//  var priceitem2 = 100;      
+// //)c
+//  var qtyitem1 = 3;        
+// // d)
+//  var qtyitem2 = 7;          
+// //) e
+//  var shippingcharges = 100; 
+
+// // Step 2: Calculate totals
+// var  subtotal = (priceitem1 * qtyitem1) + (priceitem2 * qtyitem2);
+// var  totalcost = subtotal + shippingcharges;
+
+// // Step 3: Show receipt directly on browser
+// document.write("<h2> Shopping Cart</h2>");
+// document.write(`Price item 1 is ${priceitem1}<br>`);
+// document.write(`Price item 2 is ${priceitem2}<br>`);
+// document.write(`Quantity of item 1 is is ${qtyitem1} <br>`);
+// document.write(`Quantity of item 2 is is ${qtyitem2} <br>`);
+// document.write(`Shipping Charges:</strong>${shippingcharges} <br>`);
+// document.write(`<p><strong>Total Cost:</strong> Rs.${totalcost}</p>`);
+// Q8)
+        // var TotalMarks = 980;   
+        // var MarksObtained = 804;  
+        // // Compute percentage
+        // var percentage = (MarksObtained / TotalMarks) * 100;
+        // // Show result in browser
+        // document.write("<h2>Marks Sheet</h2>");
+        // document.write(`Total Marks: ${TotalMarks}<br>`);
+        // document.write(`Obtained Marks: ${MarksObtained} <br>`);
+        // document.write(`Percentage: ${percentage.toFixed(13)}%`);
+// Q9)
+        // var usd = 10;       // US Dollars
+        // var riyal = 25;     // Saudi Riyals
+
+        // // Conversion rates
+        // var usdToPkr = 104.80;
+        // var riyalToPkr = 28;
+
+        // // Calculate total PKR in a single expression
+        // var totalPkr = (usd * usdToPkr) + (riyal * riyalToPkr);
+
+        // // Show result in browser
+        // document.write("<h1>Currency in PKR</h1>");
+        // document.write(`Total Currency in PKR: Rs. ${totalPkr}`);
+// Q10)
+// var num = 7;
+// var result =(("num + 5") *10) /2;
+// document.write("Result;", result);
+// Q11)
+// a. 
+// var currentYear = 2016;
+// // b)
+// var birthYear = 1992;
+// // c)
+// var age1 = currentYear - birthYear;
+// var total = age1 ;
+// // Show result
+// document.write("<h1>Age Calculator</h1>");
+// document.write("Current Year; "+ currentYear + "<br>");
+// document.write("Birth Year;"+ birthYear+ "<br>")
+// document.write("Your Age is; "+ age1 + "");
+// Q12)
+//   // a.
+//     var radius = 20; 
+//     // b. Calculate circumference (2 * π * r)
+//     var pi = 3.142;
+//     var circumference = 2 * pi * radius;
+//     // c. Calculate area (π * r^2)
+//     var area = pi * radius * radius;
+//     // Display results'
+//     document.write("<h1>The Geometrizer</h1>")
+//     document.write("Radius of circle:" + radius + "<br>");
+//     document.write("The circumference is: " + circumference + "<br>");
+//     document.write("The area is:" + area);
+// Q13)
+ // a)
+//     var favoriteSnack = "Chocolate Chip";
+//     // b)
+//     var currentAge = 15;
+//     // c)
+//     var maxAge = 65;
+//     // d)
+//     var amountPerDay = 3;
+//     // e)
+//     var totalNeeded = 150; 
+//     // Display result on screen
+//     document.write("<h1>The Lifetime Supply Calculator</h1>")
+//     document.write("Favorite Snack:" + favoriteSnack + "<br>");
+//     document.write("Current Age:" + currentAge + "<br>");
+//     document.write("Estimated Maximum Age:" + maxAge +"<br>");
+//     document.write("Amount Per Day:" + amountPerDay +"<br>");
+//     document.write("You will need " + totalNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
+// chap 6.
+// Q1)
+  // Step 1: Take a number in a variable
+//     var number1 = 10;
+//     // Step 3: Display result in browser
+//     document.write("Rsult:<br>")
+//     document.write("The value of a is: "+ number1 +"<br><br><br>");
+//     var a = 10;
+//   var result = ++a; 
+// document.write(" The Value of ++a: is: "+result+"<br>"); 
+// document.write("Now the value of a is: "+result+"<br><br><br>");
+// document.write("The value of a++ is:"+result+"<br>");
+// document.write("Now the value of a is:"+ ++a +"<br><br><br>");
+// document.write("The value of -a is:"+result+"<br>");
+// document.write("Now the value of a is:"+result+"<br><br><br>");
+// document.write("The value of a-- is:"+result+"<br>");
+// document.write("Now the value of a is:"+number1+"");
+
+
+
+
+
